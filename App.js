a11b15a23e83bba6020fe23fe0dec0e4f26f11e0
Godti Vinod <venkat.godti3@gmail.com>
2018-11-09 22:49:17 +0530

    Finger print from expo implemented

--- App.js
+++ App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import * as Expo from 'expo';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    this.checkDeviceForHardware();
+  }
+checkDeviceForHardware = async () => {
+    let compatible = await Expo.LocalAuthentication.hasHardwareAsync();
+    this.setState({ compatible });
+    if (!compatible) {
+      console.log("not compatible");
+    }
+  };
+showIncompatibleAlert = () => {
+    this.dropdown.alertWithType(
+      'error',
+      'Incompatible Device',
+      'Current device does not have the necessary hardware to use this API.'
+    );
+  };
   render() {
     return (
       <View style={styles.container}>
